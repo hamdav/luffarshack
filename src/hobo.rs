e4395055e64cfa1e126b8cdd38a75ce0bc691ee5
David Hambraeus <Ddhambraeus@gmail.com>
2023-05-19 21:40:30 +0200

    Structure

--- src/hobo.rs
+++ src/hobo.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 type Coord = (u32, u32);
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Mark{
     X,
     O,
@@ -11,3 +13,22 @@ struct HoboGame {
     marks: HashMap<Coord, Mark>,
     turn: Mark,
 }
+
+impl HoboGame {
+    fn move_is_legal(&self, c: Coord) -> bool {
+        // TODO
+        false
+    }
+    fn make_move(&mut self, c: Coord) -> Result {
+        //TODO
+        Ok(())
+    }
+    fn move_wins_game(&self, c: Coord) -> bool {
+        // TODO
+        false
+    }
+    fn new() -> Self {
+        Self{ marks: HashMap::new(), turn: Mark::X }
+    }
+}
+
